c0dcd37a7a1fc38acd7ba19ea0ad3dd7d3b3a998
roselandroche <r.landroche@gmail.com>
2019-09-13 20:50:54 -0400

    Got Locations going

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import TabNav from "./components/TabNav.js";
 import Header from "./components/Header.js";
 import CharacterList from "./components/CharacterList";
+import LocationsList from "./components/LocationsList";
 
 
 export default function App() {
@@ -10,6 +11,7 @@ export default function App() {
       <Header />
       {/* <TabNav /> */}
       <CharacterList />
+      <LocationsList />
     </main>
   );
 }
--- src/components/LocationCard.js
+++ src/components/LocationCard.js
@@ -1,5 +1,24 @@
 import React from "react";
+import styled from "styled-components";
 
-export default function LocationCard({ name, type, dimension, residents }) {
-  return <span>todo: location</span>;
+const StyledLocationCard = styled.div`
+  width: 45%;
+  border: 1px solid lightgrey;
+  border-radius: 8px;
+  text-align: center;
+  padding: 20px;
+  margin: 2%;
+`;
+
+function LocationCard(props) {
+  return (
+    <StyledLocationCard>
+      <h1>{props.location.name}</h1>
+      <h2>{props.location.type}</h2>
+      <h2>{props.location.dimension}</h2>
+      <h2>{`Residents:${props.location.residents.length}`}</h2>
+    </StyledLocationCard>
+  );
 }
+
+export default LocationCard
