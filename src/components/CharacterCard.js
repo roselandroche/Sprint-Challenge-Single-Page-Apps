a7379ea070a6c5c8792223d32b605103b716674b
roselandroche <r.landroche@gmail.com>
2019-09-13 21:57:54 -0400

    Small tweaks

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -13,9 +13,10 @@ function CharacterCard(props) {
   return (
     <StyledCard className='charCard'>
       <img src={props.char.image} alt={`${props.char.name} from Rick and Morty`}/>
-      <h1>{props.char.name}</h1>
-      <h3>{`${props.char.species} ${props.char.status}`}</h3>
-      <h3>{`Lives on: ${props.char.location.name}`}</h3>
+      <h1>{`Name: ${props.char.name}`}</h1>
+      <h3>{`Species: ${props.char.species}`}</h3>
+      <h3>{`Status: ${props.char.status}`}</h3>
+      <h3>{`Living in: ${props.char.location.name}`}</h3>
       <h3>{`Originally from: ${props.char.origin.name}`}</h3>
       <h3>{`In ${props.char.episode.length} Episode(s)`}</h3>
     </StyledCard>);
